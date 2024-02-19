a4f449ef34a5d2360946f77f52af84d45fcf0597
John Schulte <johannes-schulte@gmx.de>
2024-02-19 10:32:35 +0100

    basic setup

--- src/components/ResultPage.jsx
+++ src/components/ResultPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const ResultPage = () => {
   return (
-    <div>ResultPage</div>
-  )
-}
+    <div>
+      <h1>Quiz Results</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Question</th>
+            <th>Your Answer</th>
+            <th>Correct Answer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {results.map((result, index) => (
+            <tr key={index}>
+              <td>{result.question}</td>
+              <td>{result.yourAnswer}</td>
+              <td>{result.correctAnswer}</td>
+            </tr>
+          ))} */}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default ResultPage
+export default ResultPage;
