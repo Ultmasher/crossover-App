cdb2c7db4bbe126693e5630373a59ce8df8f8e53
John Schulte <johannes-schulte@gmx.de>
2024-02-19 10:08:33 +0100

    fixing bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import StartingPage from "./components/StartingPage";
+import QuizPage from "./components/QuizPage";
+import ResultPage from "./components/ResultPage";
 import "./App.css";
 
 function App() {
